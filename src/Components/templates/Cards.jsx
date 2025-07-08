41b02afd4910b24cf08479b191a870684c044c80
Rajiv004 <kumarrajiv0410@gmail.com>
2025-07-08 21:31:30 +0530

    done

--- src/Components/templates/Cards.jsx
+++ src/Components/templates/Cards.jsx
@@ -5,9 +5,9 @@ function Cards({ data , title }) {
 
 
   return (
-    <div className="w-full  flex flex-wrap gap-10 justify-center  bg-black  ">
+    <div className="w-full  flex flex-wrap gap-5 sm:gap-10 justify-center  bg-black  ">
       {data.map((c, i) => (
-        <div className="relative">
+        <div className="relative  md:flex ">
           <Link to={`/${c.media_type || title }/details/${c.id }`} className="w-[25vh]" key={i}>    
             <img
               className="shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] w-full object-cover "
@@ -17,7 +17,7 @@ function Cards({ data , title }) {
             <div className="flex justify-center items-center w-[25vh]">
               <h1 className="text-lg text-zinc-300 mt-3 font-semibold  line-clamp-1">  {c.name || c.title || c.original_name || c.original_title} </h1>
 
-              {c.vote_average && (<div className="absolute -right-3 bottom-7 text-lg font-semibold text-white w-10 h-10 bg-[#FF005C] flex justify-center items-center rounded-full ">{(c.vote_average * 10).toFixed()}<sup>%</sup></div>)}
+              {c.vote_average && (<div className="absolute -right-3 bottom-7 sm:text-lg font-semibold text-white w-7 h-7 sm:w-10 sm:h-10 bg-[#FF005C] flex justify-center items-center rounded-full ">{(c.vote_average * 10).toFixed()}<sup>%</sup></div>)}
             </div>
           </Link>
         </div>
